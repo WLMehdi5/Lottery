a7564d03b3d5b093d246c12dba4a840056710187
mehdi5 <yutas1993@gmail.com>
2019-12-09 22:16:26 +0900

    12/9 SE

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -22,9 +22,16 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField]
     private Button startButton;
 
-    bool leftStop = false;
-    bool centerStop = false;
-    bool rightStop = false;
+    [SerializeField]
+    private AudioSource one;
+    [SerializeField]
+    private AudioSource end;
+
+
+    bool leftStop = true;
+    bool centerStop = true;
+    bool rightStop = true;
+    bool birthDayMode = false;
     private List<string[]> csvList = null;
 
     private List<string> part = null;
@@ -42,43 +49,44 @@ public class NewBehaviourScript : MonoBehaviour
     {
         r = new System.Random((int)DateTime.Now.Ticks);
         csvReader();
-        
+
+        leftStop = true;
+        centerStop = true;
+        rightStop = true;
+        birthDayMode = false;
+        /*
         leftStopButton.onClick.AddListener(
             () =>
             {
-                leftStop = true;
-                left.text = part[selecter];
+                leftClick();
             }
             );
         centerStopButton.onClick.AddListener(
             () =>
             {
-                centerStop = true;
-                center.text = section[selecter];
-                
+                centerClick();             
             }
             );
          rightStopButton.onClick.AddListener(
             () =>
             {
-                rightStop = true;
-                right.text = name[selecter];
+                rightClick();
             }
             );
         startButton.onClick.AddListener(
             () =>
             {
                 reset();
-                leftStop = false;
-                centerStop = false;
-                rightStop = false;
+
             }
         );
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        // スロット化
         if (!leftStop)
         {
             left.text = part[r.Next(part.Count)];//csvList[0][r.Next(csvList[0].Length)];  //r.Next(MAX_NUMBER).ToString();
@@ -91,6 +99,53 @@ public class NewBehaviourScript : MonoBehaviour
         {
             right.text = name[r.Next(name.Count)];  //csvList[2][r.Next(csvList[2].Length)];// r.Next(MAX_NUMBER).ToString();
         }
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            reset();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            leftClick();
+            if (birthDayMode)
+            {
+                left.text = "社長！";
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            centerClick();
+            if (birthDayMode)
+            {
+                center.text = "誕生日";
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rightClick();
+            if (birthDayMode)
+            {
+                right.text = "おめでとうございます！！！";
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.B) )
+        {
+            if (leftStop && centerStop && rightStop)
+            {
+                birthDayMode = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (leftStop && centerStop && rightStop)
+            {
+                birthDayMode = false;
+            }
+        }
+
+
     }
 
     public void csvReader()
@@ -125,8 +180,52 @@ public class NewBehaviourScript : MonoBehaviour
 
     }
 
+    public void seSelect()
+    {
+        if(leftStop && centerStop && rightStop)
+        {
+            end.Play();
+            
+            return;
+        }
+        one.Play();
+    }
+
     public void reset()
     {
-        selecter = r.Next(part.Count);
+        if (leftStop && centerStop && rightStop)
+        {
+            selecter = r.Next(part.Count);
+            leftStop = false;
+            centerStop = false;
+            rightStop = false;
+        }
+    }
+
+    public void leftClick()
+    {
+        leftStop = true;
+        left.text = part[selecter];
+        part.RemoveAt(selecter);
+        seSelect();
+
+    }
+
+    public void centerClick()
+    {
+        centerStop = true;
+        center.text = section[selecter];
+        section.RemoveAt(selecter);
+        seSelect();
+
+    }
+
+    public void rightClick()
+    {
+        rightStop = true;
+        right.text = name[selecter];
+        name.RemoveAt(selecter);
+        seSelect();
+
     }
 }
